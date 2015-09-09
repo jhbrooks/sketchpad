a17815e3c18774165ce0e000f3ef3487e3d51f99
James Brooks <jhbrooks@uchicago.edu>
2015-09-09 12:22:32 -0400

    put the .square building loops into a function called drawSquares with a dimensions parameter, added variables surfaceDimensions and firstSquareDims, added a call to drawSquares taking firstSquareDims as its input

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-	var dimensions = 16;
-	for (var j = 0; j < dimensions; j++) {
-		for (var i = 0; i < dimensions; i++) {
-			$('#surface').append($("<div class='square'></div>"));
+	var surfaceDimensions = Number($('#surface').css('height').slice(0,-2));
+	var firstSquareDims = 16;
+	var drawSquares = function(dimensions) {
+		for (var j = 0; j < dimensions; j++) {
+			for (var i = 0; i < dimensions; i++) {
+				$('#surface').append($("<div class='square'></div>"));
+			};
 		};
 	};
+
+	drawSquares(firstSquareDims);
 	$(document).on('mouseenter','.square',function(){
 		$(this).css({'background-color':'#000000'});
 	});
