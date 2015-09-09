2d9fcc0bc4071e1dfc134ac1b6638ce2011381a4
James Brooks <jhbrooks@uchicago.edu>
2015-09-09 13:35:40 -0400

    made refresh its own function, asked user to re-input if newDims is not a number or over 120

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	var surfaceDimensions = Number($('#surface').css('height').slice(0,-2));
 	var firstSquareDims = 60;
+
 	var drawSquares = function(dimensions) {
 		for (var j = 0; j < dimensions; j++) {
 			for (var i = 0; i < dimensions; i++) {
@@ -11,15 +12,25 @@ $(document).ready(function(){
 		$('.square').css({'height':computedDims,'width':computedDims});
 	};
 
+	var refreshSurface = function() {
+		var newDims = prompt("Refresh with how many squares in each row?");
+		if (newDims !== null) {
+			if (isNaN(newDims)) {
+				alert("Not a number! Please try again.");
+				refreshSurface();	
+			} else if (Number(newDims) > 120) {
+				alert("Number too high! Please try again with a lower number.");
+				refreshSurface();
+			} else {
+				$('#surface').children('.square').remove();
+				drawSquares(Number(newDims));
+			};
+		};
+	};
+
 	drawSquares(firstSquareDims);
 	$(document).on('mouseenter','.square',function(){
 		$(this).css({'background-color':'#000000'});
 	});
-	$('#refresh').on('click','a',function(){
-		var newDims = prompt("Refresh with how many squares in each row?");
-		if (newDims !== null) {
-			$('#surface').children('.square').remove();
-			drawSquares(Number(newDims));
-		};
-	});
+	$('#refresh').on('click','a',refreshSurface);
 });
