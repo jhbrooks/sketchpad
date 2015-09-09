10e0d90e6cc3c20d7196d74f09a4538357af3e86
James Brooks <jhbrooks@uchicago.edu>
2015-09-09 19:14:12 -0400

    implemented rainbow button, adding and integrating rainbowOn and currentColor variables, adding a rainbowToggle function, adding a click handler for the #rainbow button, and putting conditional color randomization code into the mouseenter event handler

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	var surfaceDimensions = Number($('#surface').css('height').slice(0,-2));
 	var firstSquareDims = 60;
+	var rainbowOn = false;
+	var currentColor = '#000000';
 
 	var drawSquares = function(dimensions) {
 		for (var j = 0; j < dimensions; j++) {
@@ -32,9 +34,27 @@ $(document).ready(function(){
 		};
 	};
 
+	var rainbowToggle = function() {
+		if (rainbowOn === false) {
+			rainbowOn = true;
+		} else {
+			rainbowOn = false;
+			currentColor = '#000000';
+		};
+	};
+
 	drawSquares(firstSquareDims);
 	$(document).on('mouseenter','.square',function(){
-		$(this).css({'background-color':'#000000'});
+		if (rainbowOn === true) {
+			var rRed = Math.ceil(Math.random()*255);
+			var rGreen = Math.ceil(Math.random()*255);
+			var rBlue = Math.ceil(Math.random()*255);
+			var randomColor = "rgb("+rRed+","+rGreen+","+rBlue+")";
+			currentColor = randomColor;
+			console.log(currentColor);
+		};
+		$(this).css({'background-color':currentColor});
 	});
 	$('#refresh').on('click',refreshSurface);
+	$('#rainbow').on('click',rainbowToggle);
 });
